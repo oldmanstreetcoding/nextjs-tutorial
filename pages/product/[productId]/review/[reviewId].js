971716f5b7e61689b6e14e63bc02d1d2b38bd8ad
oldmanstreetcoding <oldmanstreetcoding@gmail.com>
2021-07-14 19:17:28 +0700

    learning dynamic nested route with adding review folder

---
+++ pages/product/[productId]/review/[reviewId].js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+
+function Review() {
+    const router = useRouter()
+    const {productId, reviewId} = router.query
+
+    return (
+        <h1>
+          Product {productId} Review {reviewId}
+        </h1>
+    )
+}
+
+export default Review
